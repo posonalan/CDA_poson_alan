7a05bee36df2a9a5f59b727c5db90314294c2c7e
alan white <alanbg59@live.fr>
2021-10-25 17:20:05 +0200

    25/10/2021 17:20:03,57

---
+++ projetCDA/c sharp/decouverte/decouverte/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace decouverte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("alan");
+            Console.Read();
+        }
+    }
+}
