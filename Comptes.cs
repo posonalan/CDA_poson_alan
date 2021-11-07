64172955087fc2e016387daace19e42c5fa6db0b
posonalan <65591159+posonalan@users.noreply.github.com>
2021-11-07 20:32:32 +0100

    travaux week end

---
+++ Comptes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte
+{
+    /* ********** Propriétaire ********** */
+    class Comptes
+    {
+   
+        public double Solde { get; set; }
+        public string Code { get; set; }
+        public Clients Proprietaire { get; set; }
+
+        /* ******** Constructeur ******** */
+        public Comptes()
+        {
+        }
+
+        public Comptes( double solde, string code, Clients proprietaire )
+        {  
+          
+            Code = code;
+            Solde = solde;
+            Proprietaire = proprietaire;
+
+        }
+       
+        public override string ToString()
+        {
+            return " \n Solde : " + this.Solde + " " + this.Code + "\n" + this.Proprietaire.ToString() ;
+        }
+
+       
+        public void Crediter(double montant)
+        {
+            Solde = Solde + montant;
+        }
+        public void Debiter(double montant)
+        {
+            Solde = Solde-montant ;
+        }
+        public string Decrire()
+        {
+            return " est de " + this.Solde +
+           " " + this.Code;
+        }
+    }
+
+}
+
---
+++ Voitures.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Applicatio_Console
+{
+    class Voitures
+    {
+        /* ********** Propriétaire ********** */
+       public string  Couleur { get ; set ; }
+        public string Marque { get ; set ; }
+        public string Modele { get ; set; }
+        public int NbKilometre { get; set; }
+        public string Motorisation { get ; set; }
+
+
+        /* ******** Constructeur ******** */
+        public Voitures()
+        {
+
+        }
+
+        public Voitures(string couleur, string marque, string modele, int nbKilometre, string motorisation)
+        {
+            Couleur = couleur;
+            Marque = marque;
+            Modele = modele;
+            NbKilometre = nbKilometre;
+            Motorisation = motorisation;
+        }
+
+        public Voitures(string marque, string modele)
+        {
+            Marque = marque;
+            Modele = modele;
+        }
+
+
+        public override string ToString()
+        {
+            return " Cette voiture est une " + this.Modele + " de la marque " + this.Marque + " , de couleur " + Couleur + " de motorisation " + Motorisation + " avec " + NbKilometre + " kilometres";
+        }
+
+        public void  Rouler(int KilometrePlus)
+        {
+            NbKilometre = NbKilometre + KilometrePlus; 
+        }
+
+          
+    }
+}
