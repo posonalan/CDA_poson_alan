e464fa24684cbd30b796647f915dec523ad5d14c
alan white <alanbg59@live.fr>
2021-11-15 10:50:27 +0100

    15/11/2021 10:50:22,35

---
+++ projetCDA/c sharp/identification_Nom_dateNaissance/identification_Nom_dateNaissance/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace identification_Nom_dateNaissance
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
