72298314877b21d4d1b71911744d222f4955f4f0
alan white <alanbg59@live.fr>
2021-11-16 10:50:23 +0100

    16/11/2021 10:50:18,62

--- projetCDA/c sharp/Calculette_v1/Calculette_v1/MainWindow.xaml.cs
+++ projetCDA/c sharp/Calculette_v1/Calculette_v1/MainWindow.xaml.cs
@@ -24,5 +24,55 @@ namespace Calculette_v1
         {
             InitializeComponent();
         }
+
+        private void BoutonNumerique_Click(object sender, RoutedEventArgs e)
+        {
+            // sender est l'element qui genere l'action 
+            tbxResultat.Text += ((Button)sender).Content;
+        }
+
+        private void BoutonInit_Click(object sender, RoutedEventArgs e)
+        {
+            tbxResultat.Text = "";
+        }
+
+        //private void CalculeFacile(object sender, RoutedEventArgs e, int valeur1, char operateur, int valeur2)
+        //{
+
+          
+            
+            //     resultat = 0;
+            //    switch (operateur)
+            //    {
+            //        case '+':
+            //            resultat = valeur1 + valeur2;
+            //            break;
+            //        case '-':
+            //            resultat = valeur1 - valeur2;
+            //            break;
+            //        case '*':
+            //            resultat = valeur1 * valeur2;
+            //            break;
+            //        case '/':
+            //            resultat = valeur1 / valeur2;
+            //            break;
+            //        case '$':
+            //            resultat = Math.Pow(valeur1, valeur2);
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //}
+
+
+        //private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    double w = Application.Current.MainWindow.Width;
+        //    double h = Application.Current.MainWindow.Height;
+        //    tbxResultat.Text = "w " + w + ", h " + h;
+        //    bouton1.FontSize = h / 20;
+        //}
+
+
     }
 }
