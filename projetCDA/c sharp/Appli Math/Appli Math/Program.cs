0958bf217e10154f9beef545bb48fbb923b77255
alan white <alanbg59@live.fr>
2021-11-17 10:50:18 +0100

    17/11/2021 10:50:14,94

---
+++ projetCDA/c sharp/Appli Math/Appli Math/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Appli_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a 
+
+           Rectangle rectangle1 = new Rectangle(a,b)
+        }
+    }
+}
---
+++ projetCDA/c sharp/Appli Math/Appli Math/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appli_Math
+{
+    class Rectangle
+    {
+        public int Longueur { get; set; }
+        public int Largeur { get; set; }
+
+
+        public Rectangle(int longueur, int largeur)
+        {
+            this.Longueur = longueur;
+            this.Largeur = largeur;
+        }
+
+
+        public int perimetre()
+        {
+            return (this.Longueur + this.Largeur)*2 ;
+
+        }
+
+        /* affichage */
+        public override string ToString()
+        {
+
+            string reponse =
+            "****_Information_****" +
+            "\n|Longueur                : " + this.Longueur +
+            "\n|Largeur           : " + this.Largeur +
+            "."; 
+          
+            return reponse;
+        }
+
+    }
+}
