7bc197ad36a98b94c4f1efda5b2514bde0f2a956
alan white <alanbg59@live.fr>
2021-11-23 12:20:47 +0100

    23/11/2021 12:20:44,64

--- projetCDA/c sharp/TestApi/TestApi/Data/MyDBContext.cs
+++ projetCDA/c sharp/TestApi/TestApi/Data/MyDBContext.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DemoBDD.Data
-{
+namespace TestApi.Data
+{ /* La classe contexte permet de créer la relation (session) avec la base de données. */ 
     public class MyDbContext : DbContext
     {
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
