21753fb79c182b0ef3f6a1f375da2268155ec1df
alan white <alanbg59@live.fr>
2021-11-23 10:50:23 +0100

    23/11/2021 10:50:18,31

---
+++ projetCDA/c sharp/TestApi/TestApi/Data/MyDBContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoBDD.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+
+        {
+
+        }
+    }
+}
