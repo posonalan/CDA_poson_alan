a76eb6f456002890967c009da8ce5d334a03c8f0
alan white <alanbg59@live.fr>
2021-12-08 17:20:08 +0100

    08/12/2021 17:20:07,25

--- BackupBdd/20211208/backup_cantinemartine_20211208.sql
+++ BackupBdd/20211208/backup_cantinemartine_20211208.sql
@@ -58,7 +58,7 @@ CREATE TABLE `categoriesproduits` (
   `IdCategorieProduit` int(11) NOT NULL AUTO_INCREMENT,
   `LibelleCategorieProduit` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`IdCategorieProduit`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -219,4 +219,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-08 10:50:25
+-- Dump completed on 2021-12-08 12:20:47
