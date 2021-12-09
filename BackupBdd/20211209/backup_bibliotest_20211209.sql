63ee70184570510b5b294722080b8ef088ce359a
alan white <alanbg59@live.fr>
2021-12-09 12:20:50 +0100

    09/12/2021 12:20:48,01

---
+++ BackupBdd/20211209/backup_bibliotest_20211209.sql
@@ -0,0 +1,160 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bibliotest
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `bibliotest`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bibliotest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `bibliotest`;
+
+--
+-- Table structure for table `biblio`
+--
+
+DROP TABLE IF EXISTS `biblio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `biblio` (
+  `IdBiblio` int(11) NOT NULL AUTO_INCREMENT,
+  `IdUtilisateur` int(11) NOT NULL,
+  `IdJeu` int(11) NOT NULL,
+  `IdConsole` int(11) NOT NULL,
+  PRIMARY KEY (`IdBiblio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `biblio`
+--
+
+LOCK TABLES `biblio` WRITE;
+/*!40000 ALTER TABLE `biblio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `biblio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `consoles`
+--
+
+DROP TABLE IF EXISTS `consoles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `consoles` (
+  `IdConsole` int(11) NOT NULL AUTO_INCREMENT,
+  `LibelleConsole` varchar(50) NOT NULL,
+  `IdMarque` int(10) NOT NULL,
+  PRIMARY KEY (`IdConsole`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `consoles`
+--
+
+LOCK TABLES `consoles` WRITE;
+/*!40000 ALTER TABLE `consoles` DISABLE KEYS */;
+INSERT INTO `consoles` VALUES (1,'Playstation',1),(2,'Nes',2);
+/*!40000 ALTER TABLE `consoles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jeux`
+--
+
+DROP TABLE IF EXISTS `jeux`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jeux` (
+  `IdJeux` int(11) NOT NULL AUTO_INCREMENT,
+  `LibelleJeu` varchar(50) NOT NULL,
+  `IdMarque` int(11) NOT NULL,
+  `IdConsole` int(11) NOT NULL,
+  PRIMARY KEY (`IdJeux`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jeux`
+--
+
+LOCK TABLES `jeux` WRITE;
+/*!40000 ALTER TABLE `jeux` DISABLE KEYS */;
+INSERT INTO `jeux` VALUES (1,'Mario',2,1),(2,'Sonic',1,2);
+/*!40000 ALTER TABLE `jeux` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marques`
+--
+
+DROP TABLE IF EXISTS `marques`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marques` (
+  `IdMarque` int(11) NOT NULL AUTO_INCREMENT,
+  `LibelleMarque` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdMarque`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marques`
+--
+
+LOCK TABLES `marques` WRITE;
+/*!40000 ALTER TABLE `marques` DISABLE KEYS */;
+INSERT INTO `marques` VALUES (1,'Sony'),(2,'Nintendo');
+/*!40000 ALTER TABLE `marques` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilisateurs` (
+  `IdUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(50) NOT NULL,
+  `Prenom` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdUtilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateurs`
+--
+
+LOCK TABLES `utilisateurs` WRITE;
+/*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
+INSERT INTO `utilisateurs` VALUES (1,'poson','alan'),(2,'poson','alan');
+/*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-09 10:50:24
