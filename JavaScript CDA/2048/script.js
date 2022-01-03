e78b815166b2f751431e41aa7e20ec341bcd76da
afpa1701871 <martine.poix@afpa.fr>
2022-01-03 15:56:26 +0100

    debug

--- JavaScript CDA/2048/script.js
+++ JavaScript CDA/2048/script.js
@@ -1,79 +1,91 @@
+//Permet de gérer  les cases vides 
+var tabCaseVide = ["case1", "case2", "case3", "case4", "case5", "case6", "case7", "case8", "case9", "case10", "case11", "case12", "case13", "case14", "case15", "case16"];
+
+InitJeu();
+compareDiv();
+
+function InitJeu() {
 
 
-
-function compareDiv(){
-
-    var divClick1 = document.getElementById("case1"); 
-    var divClick2 = document.getElementById("case2"); 
-    var additionOk = true; 
-    if ( divClick1 == divClick2 ) {
-        document.getElementById("case2").textContent = (divClick1 + divClick2);
-    additionOk == true; 
-    }
-    else{
-        additionOk = false;
-    }
+  // On place des nombres aléatoires dans toutes les cases
+  for (let i = 0; i < 16; i++) {
+    aleatoire();
+  }
 
 }
 
-function aleatoire(){
+function compareDiv() {
 
-    var chiffreAleatoire =  Math.random() * (4 - 2) + 2 ;
-
-    var case1 = document.getElementById("case1"); 
-
-    document.getElementById("case1").textContent = chiffreAleatoire; 
-    
+  var divClick1 = document.getElementById("case1");
+  var divClick2 = document.getElementById("case2");
+  var additionOk = true;
+  if (divClick1.textContent == divClick2.textContent) {
+    divClick2.textContent = (parseInt(divClick1.textContent) + parseInt(divClick2.textContent));
+    additionOk == true;
+  }
+  else {
+    additionOk = false;
+  }
+  alert(additionOk);
 }
 
-function random(min, max){
-    return Math.floor(Math.random() * (max - min) + min)
-    
+function aleatoire() {
+
+  // math.random donne un nombre entre 0 et 1
+  var chiffreAleatoire = Math.random() > 0.5 ? 2 : 4;
+  //math.floor renvoi le nombre entier plus petit  (tronquer)
+  var numCaseAleatoire = Math.floor(Math.random() * tabCaseVide.length);
+  // On recupere la case associé au numero aleatoire
+  var caseAlea = document.getElementById(tabCaseVide[numCaseAleatoire]);
+  // on met le chiffre aleatoire dans la case
+  caseAlea.textContent = chiffreAleatoire;
+  // on retire la case du tableau des cases vides
+  tabCaseVide.splice(numCaseAleatoire, 1);
 }
 
-function score(){
 
-    var scoreDebut = document.getElementById("score"); 
-    
-     scoreDebut.innerHTML++ ; 
 
-    document.getElementById("score").innerHTML = scoreDebut; 
+function majScore() {
+
+  var scoreDebut = document.getElementById("score");
+  var score = scoreDebut.innerHTML;
+
+  scoreDebut.innerHTML = ++ score ;
 }
 
-function timer(){
+function timer() {
 
-// const departMinutes = 5
-// let temps = departMinutes * 60
+  // const departMinutes = 5
+  // let temps = departMinutes * 60
 
-const departSeconde = 10
-let temps = departSeconde 
-const timerElement = document.getElementById("timer")
+  const departSeconde = 10
+  let temps = departSeconde
+  const timerElement = document.getElementById("timer")
 
-setInterval(() => {
-//   let minutes = parseInt(temps / 60, 10)
-  let secondes = parseInt(temps % 60, 10)
+  setInterval(() => {
+    //   let minutes = parseInt(temps / 60, 10)
+    let secondes = parseInt(temps % 60, 10)
 
-//   minutes = minutes < 10 ? "0" + minutes : minutes
-  secondes = secondes < 10 ? "0" + secondes : secondes
+    //   minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
 
-//   timerElement.innerText = `${minutes}:${secondes}`
-  timerElement.innerText = `${secondes}`
+    //   timerElement.innerText = `${minutes}:${secondes}`
+    timerElement.innerText = `${secondes}`
 
-  temps = temps <= 0 ? 0 : temps - 1
-}, 1000)
+    temps = temps <= 0 ? 0 : temps - 1
+  }, 1000)
 
 }
 
-function reinitTimer()
-{
-    if ( addiditionOk == true ){
-      timer(); 
-    }
+function reinitTimer() {
+  if (addiditionOk == true) {
+    timer();
+  }
 
-    else {
-        timer(0);
-        console.log("Game over"+score); 
-    }
+  else {
+    timer(0);
+    console.log("Game over" + score);
+  }
 
 }
 
