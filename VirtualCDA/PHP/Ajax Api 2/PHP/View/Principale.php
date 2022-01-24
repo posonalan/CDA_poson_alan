0daa11795ad7fa5bc41bd11d9a31ee3c72f23b30
posonalan <alan.poson@gmail.com>
2022-01-24 17:25:29 +0100

    fin journée

---
+++ VirtualCDA/PHP/Ajax Api 2/PHP/View/Principale.php
@@ -0,0 +1,16 @@
+﻿
+<div class="meteo">
+    <div class="contient">
+        <div class="cadre">
+            <div class="colonneM">
+                <!-- Titre -->
+                <h1 class="textM">bibliotheque</h1>
+               <div id="libelleValue"></div>
+                 <div id="value"></div>     
+<div id="howMany"></div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
