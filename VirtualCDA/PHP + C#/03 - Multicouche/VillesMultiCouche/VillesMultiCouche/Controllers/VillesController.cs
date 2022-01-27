32bb3d9c8f652b1aa4863f147c1399b76fe6d331
alan.poson@gmail.com <alan.poson@gmail.com>
2022-01-27 16:21:09 +0100

    push 16h30

---
+++ VirtualCDA/PHP + C#/03 - Multicouche/VillesMultiCouche/VillesMultiCouche/Controllers/VillesController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VillesMultiCouche.Data.Dtos;
+using VillesMultiCouche.Data.Models;
+using VillesMultiCouche.Data.Services;
+
+namespace VillesMultiCouche.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class VillesController : ControllerBase
+    {
+        private readonly VillesServices _service;
+        private readonly IMapper _mapper;
+
+        public VillesController(VillesServices service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        //GET api/Villes
+        [HttpGet]
+        [EnableCors("toto")]
+        public ActionResult<IEnumerable<VilleDTO>> GetAllVilles()
+        {
+            IEnumerable<Ville> listeVilles = _service.GetAllVilles();
+            return Ok(_mapper.Map<IEnumerable<VilleDTO>>(listeVilles));
+        }
+
+        //GET api/Villes/{i}
+        [HttpGet("{id}", Name = "GetVillesById")]
+        [EnableCors("toto")]
+        public ActionResult<VilleDTO> GetVillesById(int id)
+        {
+            Ville commandItem = _service.GetVilleById(id);
+            if (commandItem != null)
+            {
+                return Ok(_mapper.Map<VilleDTO>(commandItem));
+            }
+            return NotFound();
+        }
+
+        //POST api/Villes
+        [HttpPost]
+        [EnableCors("toto")]
+        public ActionResult<VilleDTO> CreateVilles(Ville obj)
+        {
+            _service.AddVille(obj);
+            return CreatedAtRoute(nameof(GetVillesById), new { Id = obj.IdVille }, obj);
+        }
+
+        //POST api/Villes/{id}
+        [HttpPut("{id}")]
+        [EnableCors("toto")]
+        public ActionResult UpdateVilles(int id, VilleDTO obj)
+        {
+            Ville objFromRepo = _service.GetVilleById(id);
+            if (objFromRepo == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(obj, objFromRepo);
+            _service.UpdateVille(objFromRepo);
+            return NoContent();
+        }
+
+
+        //DELETE api/Villes/{id}
+        [HttpDelete("{id}")]
+        [EnableCors("toto")]
+        public ActionResult DeleteVilles(int id)
+        {
+            Ville obj = _service.GetVilleById(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _service.DeleteVille(obj);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ VirtualCDA/PHP + C#/03 - Multicouche/Web/ajax.js
@@ -0,0 +1,51 @@
+var contenu = document.querySelector(".contenu");
+const requ = new XMLHttpRequest();
+
+/**** GET */
+requ.open('GET', 'https://localhost:44398/api/Departements', true);
+requ.send();
+
+/**** GET by ID  */
+// requ.open('GET', 'https://localhost:44398/api/Departements/1', true);
+// requ.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// requ.send();
+
+
+// /**** POST */
+// requ.open('POST', 'https://localhost:44398/api/Departements', true);
+// requ.setRequestHeader("Content-Type", "application/json");
+// var args={
+//     "idDepartement": 0,
+//     "libelle": "nouveau"
+//   }
+//   requ.send(JSON.stringify(args));
+
+//   /**** PUT */
+//   requ.open('PUT', 'https://localhost:44398/api/Departements/1', true);
+//   requ.setRequestHeader("Content-Type", "application/json");
+//   var args={
+//       "idDepartement": 1,
+//       "libelle": "test"
+//     }
+//     requ.send(JSON.stringify(args));
+
+
+// /**** Delete  */
+// requ.open('DELETE', 'https://localhost:44398/api/Departements/6', true);
+// requ.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// requ.send();
+
+
+requ.onreadystatechange = function(event) {
+    if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200) {
+            console.log("Réponse reçue: %s", this.responseText);;
+            console.log(this.responseText);
+            reponse=JSON.parse(this.responseText);
+            
+            
+        } else {
+            console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+        }
+    }
+};
